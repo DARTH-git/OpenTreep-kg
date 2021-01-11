96c3ea9bc6f32139c40515e1fedbb4157841c144
MarianaFdz <marianafdz465@gmail.com>
2021-01-11 15:18:02 -0600

    Test

--- R/runExample.R
+++ R/runExample.R
@@ -1,5 +1,6 @@
 #' @export
-runExample <- function() {
+runExample <- function(fileName) {
+  fileName <<- fileName
   appDir <- system.file("myapp", package = "OpenTree")
   if (appDir == "") {
     stop("Could not find myapp. Try re-installing `mypackage`.", call. = FALSE)
