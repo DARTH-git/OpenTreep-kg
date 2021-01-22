8549af93e79ead80024e5a1357d474acf00e33e9
Mariana Fernandez <56494627+marianafdz465@users.noreply.github.com>
2021-01-22 16:53:16 -0600

    Update createTree.R

--- R/createTree.R
+++ R/createTree.R
@@ -33,7 +33,9 @@ createTree <- function(fileName, dirname, libpath) {
   }
   #shiny::runApp(appDir, display.mode = "normal")
   # jobRunScript("inst/myapp/shiny-run.R", importEnv = TRUE)
-  jobRunScript(paste0(libpath, "/OpenTree/myapp/shiny-run.R"), importEnv = TRUE)
+  path_shinyrun <- paste0(appDir, "/shiny-run.R")
+  jobRunScript(path_shinyrun, importEnv = TRUE)
+  #jobRunScript(paste0(libpath, "/OpenTree/myapp/shiny-run.R"), importEnv = TRUE)
   #path_aux2 <- paste0(appDir, "/shiny-run.R")
   #source(path_aux2)
   # return(path_aux)
